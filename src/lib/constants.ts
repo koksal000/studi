6d13836e6ab9c772b910e61877ca8a067a55ea9b
Domaniç Çamlıca Köyü <domaniccamlicakoyu@gmail.com>
2025-05-20 10:29:59 +0000

    1. Görüntüyü kaldır ve hepsini kaldır ve sana verdiğim linkleri "sondan başa" doğru ekle

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -56,17 +56,18 @@ export const TIMELINE_EVENTS = [
 
 // This STATIC_GALLERY_IMAGES_FOR_SEEDING can be used for initial seeding of the dynamic gallery if it's empty on server start.
 // The live gallery will be managed via the admin panel and API.
+// Images are from ibb.co, ordered "sondan başa" as per user request.
 export const STATIC_GALLERY_IMAGES_FOR_SEEDING = [
-  { id: "seed_1", src: "https://static.daktilo.com/sites/1410/uploads/2024/07/16/amlica-ilgenel3-1721118756.webp", alt: "Çamlıca Köyü Camii", caption: "Çamlıca Köyü Camii", hint: "mosque historic" },
-  { id: "seed_2", src: "https://i.ibb.co/s9mGgssQ/1739026216106.jpg", alt: "Çamlıca Uydu Görüntüsü", caption: "Çamlıca Uydu Görüntüsü", hint: "village satellite" },
-  { id: "seed_3", src: "https://i.ibb.co/XhVTv1f/1739026216113.jpg", alt: "Çamlıca Tarım Kredi Kooperatifi", caption: "Çamlıca Tarım Kredi Kooperatifi", hint: "village cooperative" },
-  { id: "seed_4", src: "https://i.ibb.co/5xCg3hcC/1739026216091.jpg", alt: "Çamlıca Köyü Ortaokulu", caption: "Çamlıca Köyü Ortaokulu", hint: "village school" },
-  { id: "seed_5", src: "https://i.ibb.co/7xdY3gcL/1739026216097.jpg", alt: "Çamlıca Köyü Çay Konağı", caption: "Çamlıca Köyü Çay Konağı", hint: "tea house" },
-  { id: "seed_6", src: "https://i.ibb.co/Y5Dt0Rg/1739027099716.jpg", alt: "Çamlıca Köyü Mezarlığı Girişi", caption: "Çamlıca Köyü Mezarlığı Girişi", hint: "cemetery entrance" },
-  { id: "seed_7", src: "https://i.ibb.co/1JwKQkQN/1739026216083.jpg", alt: "Çamlıca Köyü Girişi", caption: "Çamlıca Köyü Girişi", hint: "village entrance" },
-  { id: "seed_8", src: "https://i.ibb.co/N6ZvLqsJ/1739026216071.jpg", alt: "Çamlıca Köyü Göleti", caption: "Çamlıca Köyü Göleti", hint: "village pond" },
-  { id: "seed_9", src: "https://i.ibb.co/SD8rnMj3/Whats-App-G-rsel-2025-02-08-saat-16-20-29-6ff8c8bd.jpg", alt: "Çamlıca Köyü Girişi 2", caption: "Çamlıca Köyü Girişi 2", hint: "village entrance" },
-  { id: "seed_10",src: "https://i.ibb.co/NnYV58Gg/1739026216056.jpg", alt: "Yaşar Gıda Market", caption: "Yaşar Gıda Market", hint: "village market" },
+  { id: "seed_1", src: "https://i.ibb.co/s9mGgssQ/1739026216106.jpg", alt: "1739026216106", caption: "Çamlıca Köyü Camii", hint: "mosque village" },
+  { id: "seed_2", src: "https://i.ibb.co/XhVTv1f/1739026216113.jpg", alt: "1739026216113", caption: "Çamlıca Uydu Görüntüsü", hint: "village satellite" },
+  { id: "seed_3", src: "https://i.ibb.co/5xCg3hcC/1739026216091.jpg", alt: "1739026216091", caption: "Çamlıca Tarım Kredi Kooperatifi", hint: "village cooperative" },
+  { id: "seed_4", src: "https://i.ibb.co/7xdY3gcL/1739026216097.jpg", alt: "1739026216097", caption: "Çamlıca Köyü Ortaokulu", hint: "village school" },
+  { id: "seed_5", src: "https://i.ibb.co/Y5Dt0Rg/1739027099716.jpg", alt: "1739027099716", caption: "Çamlıca Köyü Çay Konağı", hint: "tea house" },
+  { id: "seed_6", src: "https://i.ibb.co/1JwKQkQN/1739026216083.jpg", alt: "1739026216083", caption: "Çamlıca Köyü Mezarlığı Girişi", hint: "cemetery entrance" },
+  { id: "seed_7", src: "https://i.ibb.co/N6ZvLqsJ/1739026216071.jpg", alt: "1739026216071", caption: "Çamlıca Köyü Girişi", hint: "village entrance" },
+  { id: "seed_8", src: "https://i.ibb.co/SD8rnMj3/Whats-App-G-rsel-2025-02-08-saat-16-20-29-6ff8c8bd.jpg", alt: "Whats-App-G-rsel-2025-02-08-saat-16-20-29-6ff8c8bd", caption: "Çamlıca Köyü Göleti", hint: "village pond" },
+  { id: "seed_9", src: "https://i.ibb.co/PsmMVdCN/1739026216077.jpg", alt: "1739026216077", caption: "Çamlıca Köyü Girişi 2", hint: "village entrance" },
+  { id: "seed_10",src: "https://i.ibb.co/NnYV58Gg/1739026216056.jpg", alt: "1739026216056", caption: "Yaşar Gıda Market", hint: "village market" },
 ];
 
 
@@ -84,5 +85,3 @@ export const NAVIGATION_LINKS = [
 
 export const ADMIN_PANEL_PATH = "/admin";
 export const DEFAULT_CONTACT_PEER_ID = "camlica-village-default-contact-peer";
-
-    
